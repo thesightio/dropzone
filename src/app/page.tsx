3bf8d1b5f513612377daacd29048b9d4c2f4bb70
Nikita <ns@bounds.agency>
2023-08-13 04:57:06 +0200

    Add parameters for custom init Dropzone

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,16 @@ import { Flex, Text } from '@chakra-ui/react'
 import Dropzone from "@/components/dropZone/dropZone"
 
 export default function Home() {
+  const availableTypes = [
+      'image/jpeg',
+      'image/png',
+      'image/gif',
+      'image/webp'
+  ]
   return (
     <>
       <Flex justify="center" align="center" w="100%" h="100%" direction="column">
-          <Dropzone/>
+          <Dropzone maxSize={3} availableTypes={availableTypes}/>
           <Text marginTop="20px" opacity="0.5" width="600px" align="center">Изображения должны соответствовать указанным форматам и не превышать размер в 3мб.</Text>
       </Flex>
     </>
